46940473e541f52034b3e242b222c8f599b0f3e8
ZayK12 <27kazi-zayyaan@jonesboroschools.net>
2024-08-27 11:03:35 -0500

    Add files via upload
    Added more comments, still needs more comments, need to rename variables, and fixed the variables for x-drive turning.

--- main.cpp
+++ main.cpp
@@ -37,7 +37,7 @@ motor BackRight = motor(PORT2, ratio18_1, true);
 
 inertial Inertial4 = inertial(PORT4);
 
-
+//Sides for tank drive, corners on X-Drive
 motor_group leftside = motor_group(FrontLeft, BackLeft);
 motor_group rightside = motor_group(FrontRight, BackRight);
 
@@ -47,7 +47,8 @@ bool RemoteControlCodeEnabled = true;
 competition Competition;
 int Brain_precision = 0, Console_precision = 0;
 
-int TV = 0/(3.14159 * 104.775) * 360;
+int TV = 0/(3.14159 * 104.775) * 360; // Targeted Value
+// The calculation is used to convert mm to degrees.
 /*
 float MoveLateral(float x)
 {
@@ -57,11 +58,15 @@ float MoveLateral(float x)
   return 0;
 }
 */
+// Tank Drive turning X-Drive Side to side movement.
 int turninput = 0;
 
+//X-Drive Turning, may work with mechan wheels (Untested)
 int actualturning = 0;
 
+//swtch toggles the PID calculations and spinning the motors
 bool swtch = true; // switch to toggle while loop
+//swtch2 toggles updating all PID variables
 bool swtch2 = true; // debug switch
 
 
@@ -106,24 +111,37 @@ float integral = 0; //
 float derivative = 0; // error - prev_error : 
 float averagerot = 0; //
 
+//getVar event is used to multithread the code
 event getVar = event();
+//reset event is ued to reset all PID calculations.
 event reset = event();
+
+//PID variables attached to the turninput variable
 float turnV = 0;
 float turnError = .1; //error is the distance left from the current position to the TV
 float turnIntegral = 0;
 float turnDerivative = 0;
 float turnPrev_error = 0;
 
-
+//Average Right Motor Position
 float AVGRMP;
+
+//Average Left Motor Position
 float AVGLMP;
+
+//Unused all clear boolean for resettting
 bool AC = false;
-float actualturningV = 0;
+
+
+//actual turning is for x-drive turning as turn causes side to side movement
+float actualturningV = 0; //Voltage to give motors
 float actualturningIntegral = 0;
 float actualturningDerivative = 0;
 float actualturningPrev_error = 0;
 float actualturningError = 0;
 float turningrot = 0;
+
+
 #pragma endregion Declarables 
 
 
@@ -140,6 +158,7 @@ void VEXcode_driver_task() {
   return;
 }
 
+//Reset1 sets all motor rotation sensors to 0 making the PID think that it is at its destination
 void reset1()
 {
   FrontRight.setPosition(0, degrees);
@@ -148,6 +167,7 @@ void reset1()
   BackLeft.setPosition(0, degrees);
 }
 #pragma region SIDPID
+
 
 // Left calculation
 void onevent_getVar_0()
@@ -195,6 +215,7 @@ void onevent_getVar_2()
   
 }
 
+//Calculation used for X-Drive turning.
 void onevent_getVar_3()
 {
   turningrot = abs(FrontRight.position(degrees)) + abs(BackRight.position(degrees));
@@ -203,8 +224,8 @@ void onevent_getVar_3()
   {
     actualturningIntegral = 0;
   }
-  actualturningIntegral += turnError;
-  turnPrev_error = turnError;
+  actualturningIntegral += actualturningError;
+  actualturningPrev_error = actualturningError;
 
 }
 
@@ -243,7 +264,7 @@ void whenstarted1()
 }
 int whenStarted3()
 {
-  while (swtch2)
+  while (swtch)
   {
     actualturningV = actualturningKp*actualturningError +actualturningKi*actualturningIntegral + actualturningKd * (actualturningError - actualturningPrev_error);
     FrontRight.spin(forward, actualturningV, voltageUnits::volt);
